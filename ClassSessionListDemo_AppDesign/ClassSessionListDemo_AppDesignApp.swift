54962bf45b065ce84a3c0af6da1674ac0e44523e
Ameya-Joshi79 <45701553+Ameya-Joshi79@users.noreply.github.com>
2023-02-21 18:15:21 -0500

    Initial Commit

---
+++ ClassSessionListDemo_AppDesign/ClassSessionListDemo_AppDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassSessionListDemo_AppDesignApp.swift
+//  ClassSessionListDemo_AppDesign
+//
+//  Created by Ameya Joshi on 2023-02-21.
+//
+
+import SwiftUI
+
+@main
+struct ClassSessionListDemo_AppDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
